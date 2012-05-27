e37793d5776a44130fba6f51c563830e94614aa6
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-05-27 21:56:37 +0900

    initial commit

---
+++ cookbooks/git_commiter/attributes/default.rb
@@ -0,0 +1,4 @@
+
+default[:git_commiter][:user]  = 'root'
+
+default[:git_commiter][:cron][:push]  = 
---
+++ cookbooks/git_commiter/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Dmytro Kovalov"
+maintainer_email "dmytro.kovalov@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures git_commiter"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
